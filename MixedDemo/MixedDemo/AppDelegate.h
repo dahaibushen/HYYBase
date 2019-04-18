59cea82aa92e8bfe256239d7f33350025be4f739
hu <>
2019-04-18 22:14:47 +0800

    创建项目

---
+++ MixedDemo/MixedDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MixedDemo
+//
+//  Created by hu on 2019/4/18.
+//  Copyright © 2019 hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MixedDemo/MixedDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MixedDemo
+//
+//  Created by hu on 2019/4/18.
+//  Copyright © 2019 hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
